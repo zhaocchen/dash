aa31722d5ea7461c2a77289761adbcd186847983
dasheng <zhaocchen@163.com>
2018-02-07 11:10:25 +0800

    test babel

---
+++ babel/js/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var name = 'Scottee';
+var greeting = 'hello ' + name;
+console.log(greeting);
+
+/** 
+ * 1. npm install -g babel-cli
+ * 2. npm init -yes
+ * 3. npm install babel-preset-es2015 --save-dev
+ * 4. run ：babel es6/js --out-file js/test.js --presets es2015
+ */
