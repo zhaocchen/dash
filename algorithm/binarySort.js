52d8aa1e20b835471f74930486e8f4c68792cc03
dasheng <zhaocchen@163.com>
2018-06-12 20:44:35 +0800

    traver binary tree

--- algorithm/binarySort.js
+++ algorithm/binarySort.js
@@ -41,6 +41,74 @@ const binarySort = function(data) {
 	return binaryTree;
 } 
 
-	console.log(binarySort(testData));
+/**
+ * 遍历二叉树
+ * return Array
+ */
+const traverseTree = {
+	tmpArray: [],
+	push(key) {
+		this.tmpArray.push(key);
+	},
+	inOrderNode(node, callback) {
+		if(node !== null) {
+			this.push(node.key);
+			this.inOrderNode(node.left, this.push);
+			this.inOrderNode(node.right, this.push);
+		}
+	},
+	/**
+	 * 执行中序遍历
+	 * @param {*} binaryTree 
+	 */
+	inOrder(binaryTree) {
+		this.tmpArray = [];
+		this.inOrderNode(binaryTree, this.push);
+		return this.tmpArray;
+	},
+	preOrderNode(node, callback) {
+		if(node !== null) {
+			this.inOrderNode(node.left, this.push);
+			this.push(node.key);
+			this.inOrderNode(node.right, this.push);
+		}
+	},
+	/**
+	 * 执行前序遍历
+	 * @param {*} binaryTree 
+	 */
+	preOrder(binaryTree) {
+		this.tmpArray = [];
+		this.preOrderNode(binaryTree, this.push);
+		return this.tmpArray;
+	},
+	postOrderNode(node, callback) {
+		if(node !== null) {
+			this.inOrderNode(node.left, this.push);
+			this.inOrderNode(node.right, this.push);
+			this.push(node.key);
+		}
+	},
+	/**
+	 * 执行后序遍历
+	 * @param {*} binaryTree 
+	 */
+	postOrder(binaryTree) {
+		this.tmpArray = [];
+		this.postOrderNode(binaryTree, this.push);
+		return this.tmpArray;
+	},
+};
 
+const tree = binarySort(testData);
+// console.log(tree);
+
+const inOrderRes = traverseTree.inOrder(tree);
+console.log('inOrder',inOrderRes);
+
+const preOrderRes = traverseTree.preOrder(tree);
+console.log('preOrder',preOrderRes);
+
+const postOrderRes = traverseTree.postOrder(tree);
+console.log('postOrder',postOrderRes);
 
