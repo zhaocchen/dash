111a9d13ebaa8616d1e08f1ba927e3e33e48204b
daaasheng <zhaocchen@163.com>
2018-08-13 23:25:14 +0800

    interview of MI

---
+++ python/testcard.py
@@ -0,0 +1,14 @@
+# len
+len = 5
+indexArr = []
+arr = [len]
+
+for i in range(1, len):
+    indexArr.append(len-i)
+    if i > 0:
+        tmp = arr.pop()
+        arr.insert(0, tmp)
+        arr.insert(0, len-i)
+
+print indexArr
+print arr
