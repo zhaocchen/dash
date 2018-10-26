fd89e6183298f6c0ae3b47f02a04705635a9f3d6
dasheng <zhaocchen@163.com>
2018-10-26 22:49:25 +0800

    add leetcode

--- py/leet/leet344.py
+++ py/leet/leet344.py
@@ -2,9 +2,10 @@
 # type x:str
 # rtype: str
 
-x = raw_input("input a string:")
-# x = "A man, a plan, a canal: Panama"
+# x = raw_input("input a string:")
+x = "A man, a plan, a canal: Panama"
 
+a = x
 b = list(reversed(list(a)))
 b = "".join(b)
 
