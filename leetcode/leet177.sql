39191da8b30ab474f23954053271ea1625b71d49
daaasheng <zhaocchen@163.com>
2020-06-10 11:16:24 +0800

    seoaration leetcode into new repo

--- leetcode/leet177.sql
+++
@@ -1,8 +0,0 @@
-CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
-BEGIN
-  SET N = N - 1;
-  RETURN (
-      # Write your MySQL query statement below.
-      SELECT DISTINCT Salary  FROM Employee ORDER By Salary DESC LIMIT N, 1
-  );
-END
