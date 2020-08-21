2802fe8f38e919ef718ec3c3324498c6e260d934
daaasheng <zhaocchen@163.com>
2020-08-21 14:51:25 +0800

    rm base

--- base/iframe/utils.js
+++
@@ -1,7 +0,0 @@
-function isInFrame () {
-    if (window == top) { // current window == window.top?
-        alert('The script is in the topmost window, not in a frame');
-      } else {
-        alert('The script runs in a frame!');
-      }
-}
